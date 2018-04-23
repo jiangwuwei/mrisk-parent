f1c4d620674449a306e202a3eb47b4c7f0ce169f
yulin.jiang <yulin.jiang@bkjk.com>
2018-04-23 11:19:00 +0800

    polish

--- mrisk-operating/moperating-web/src/db/20170419_monitor_operating/InsertIntoLejrDim.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `zoom_dim` VALUES ('MonitorBusiType', '1', '风控系统');
-INSERT INTO `zoom_dim` VALUES ('MonitorBusiType', '2', '授信系统');
-INSERT INTO `zoom_dim` VALUES ('MonitorBusiType', '9', '其他系统');
-
-alter table zoom_schedule_job AUTO_INCREMENT=10000;
